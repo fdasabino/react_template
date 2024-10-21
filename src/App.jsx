e36e74f6dbcd23a826bc572e6890933d00158a47
Francisco Sabino <fdasabino@gmail.com>
2024-10-21 14:01:10 +0200

    Added sass modules

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styles from "./index.module.scss";
+
+function App() {
+  return (
+    <div className={styles.app__container}>
+      <div className={styles.app__container_header}>
+        <h1>Template react app</h1>
+      </div>
+      <h1>Second heading</h1>
+    </div>
+  );
+}
+
+export default App;
